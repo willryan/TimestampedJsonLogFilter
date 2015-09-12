584d0c3efe534fd5a2b5e7fe93b6270558844db8
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2015-09-11 21:06:14 -0400

    update versions, no requirement of specific JSON.NET

--- src/TimestampedJsonLogFilter/AssemblyInfo.fs
+++ src/TimestampedJsonLogFilter/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("TimestampedJsonLogFilter")>]
 [<assembly: AssemblyProductAttribute("TimestampedJsonLogFilter")>]
 [<assembly: AssemblyDescriptionAttribute("filters timestamped json logs")>]
-[<assembly: AssemblyVersionAttribute("0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.1")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1"
+    let [<Literal>] Version = "1.0"
