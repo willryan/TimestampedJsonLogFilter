b05f6b5f9903fa074eb9ce5cb207924575822fc7
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2015-09-11 21:29:42 -0400

    another try at nuget

--- src/TimestampedJsonLogFilter/AssemblyInfo.fs
+++ src/TimestampedJsonLogFilter/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("TimestampedJsonLogFilter")>]
 [<assembly: AssemblyProductAttribute("TimestampedJsonLogFilter")>]
 [<assembly: AssemblyDescriptionAttribute("filters timestamped json logs")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.5")>]
+[<assembly: AssemblyFileVersionAttribute("0.5")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
+    let [<Literal>] Version = "0.5"
